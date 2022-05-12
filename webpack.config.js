59f461eccbd237d2794a23446eb93922830e00d4
reda777 <redazara1998@gmail.com>
2022-05-12 12:20:42 +0100

    Initial set up

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+
+        new HtmlWebpackPlugin({
+
+            title: 'Todo List',
+
+        }),
+
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
